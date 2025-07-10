155b25f4a294d1d34bd774f1eda0aa13f668ca83
FCL00 <flagahit00@gmail.com>
2025-07-10 17:53:27 +0800

    feature: create a routes for all the pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import HomePage from '@/views/HomePage.vue'
+import ProductDetails from '@/views/ProductDetails.vue'
+import Product from '@/views/ProductsPage.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: MainLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+      ],
+    },
+
+    {
+      path: '/products',
+      component: MainLayout,
+      children: [
+        {
+          path: '/products',
+          name: 'products',
+          component: Product,
+        },
+        {
+          path: '/products/:id',
+          name: 'products-details',
+          component: ProductDetails,
+        },
+      ],
     },
   ],
 })
