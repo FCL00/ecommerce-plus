769c47879f9d40653e2e8b6cdf1d3a77a5e20c1f
FCL00 <flagahit00@gmail.com>
2025-07-11 17:34:54 +0800

    fix: product view layout

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container">
+    <product-list />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import { ProductList } from '@/components'
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+}
+</style>
